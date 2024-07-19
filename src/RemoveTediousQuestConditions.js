4a66c4df05278316ad507a668d2006f930f780ef
Lauri <100446318+ppyLEK@users.noreply.github.com>
2024-07-19 10:42:55 +0300

    Upload 'updated' mod files

---
+++ src/RemoveTediousQuestConditions.js
@@ -0,0 +1,97 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class RemoveTediousQuestConditions {
+    container;
+    config = require("../config/config.json");
+    logger;
+    loggerBuffer = [];
+    postDBLoad(container) {
+        this.container = container;
+        this.logger = this.container.resolve("WinstonLogger");
+        const quests = this.container.resolve("DatabaseServer").getTables().templates.quests;
+        const conditionsToRemove = [];
+        const LogTextColor_1 = require("C:/snapshot/project/obj/models/spt/logging/LogTextColor");
+        for (let eachOption in this.config) {
+            if (this.config[eachOption] != false) {
+                switch (eachOption) {
+                    case 'debug':
+                        break;
+                    case 'setPMC':
+                        quests['5a27c99a86f7747d2c6bdd8e'].conditions.AvailableForFinish[0].counter.conditions[0].target = 'AnyPmc';
+                        break;
+                    case 'Location':
+                    case 'InZone':
+                    case 'VisitPlace':
+                    case 'Shots':
+                    case 'HealthEffect':
+                        this.findCounterCreators(eachOption, quests);
+                        break;
+                    default:
+                        conditionsToRemove.push(eachOption);
+                        break;
+                }
+            }
+        }
+        conditionsToRemove.forEach(condition => {
+            if (this.config.debug === true)
+                this.logger.info(`\n${condition}`);
+            for (let eachQuest in quests)
+                this.keyClearer(quests[eachQuest], condition, eachQuest, quests);
+            if (this.config.debug === true && this.loggerBuffer.length > 0)
+                this.logger.info([...new Set(this.loggerBuffer)]);
+            this.loggerBuffer = [];
+        });
+    }
+    /**
+     * Recursive function that clears a given keys value to [] in a nested object
+     * @param object object to search thorugh
+     * @param key key to find
+     * @param questId questId for logging
+     * @param quests container/database/quests
+     */
+    keyClearer(object, key, questId, quests) {
+        if (Object.prototype.hasOwnProperty.call(object, key)) {
+            key !== 'distance' ? object[key] = [] : object[key].value = 0;
+            this.loggerBuffer.push(quests[questId].QuestName);
+        }
+        for (var i = 0; i < Object.keys(object).length; i++) {
+            if (typeof object[Object.keys(object)[i]] == 'object') {
+                this.keyClearer(object[Object.keys(object)[i]], key, questId, quests);
+            }
+        }
+    }
+    /**
+     * Passes any quests with _parent of CounterCreator to removeNested()
+     * @param target key to search for
+     * @param quests container/database/quests
+     */
+    findCounterCreators(target, quests) {
+        if (this.config.debug === true)
+            this.logger.info(`\n${target}`);
+        for (let eachQuest in quests) {
+            for (let eachCondition in quests[eachQuest].conditions.AvailableForFinish) {
+                if (quests[eachQuest].conditions.AvailableForFinish[eachCondition].conditionType === 'CounterCreator')
+                    this.removeNested(quests[eachQuest].conditions.AvailableForFinish[eachCondition], target, quests[eachQuest].QuestName);
+            }
+        }
+        if (this.config.debug === true && this.loggerBuffer.length > 0)
+            this.logger.info([...new Set(this.loggerBuffer)]);
+        this.loggerBuffer = [];
+    }
+    /**
+     * Finds object with parent of target and removes
+     * @param input each condition in CounterCreator
+     * @param target the _parent condition to find
+     * @param questName the name fo the quest for logging
+     */
+    removeNested(input, target, questName) {
+        let thisCondition = input.counter.conditions;
+        thisCondition.filter((finalCondition) => finalCondition.conditionType === target)
+            .forEach((finalCondition) => {
+            thisCondition.splice(thisCondition.indexOf(finalCondition), 1);
+            this.loggerBuffer.push(questName);
+        });
+    }
+}
+module.exports = { mod: new RemoveTediousQuestConditions() };
+//# sourceMappingURL=RemoveTediousQuestConditions.js.map
